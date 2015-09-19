ba8e1aed7cd8bf5ff5144a77c1e1a0d18a47f7d8
Michael Jennings <michael.jennings91@gmail.com>
2015-09-19 23:29:58 +0100

    Added comments to the config file

--- config/coreplex.php
+++ config/coreplex.php
@@ -4,13 +4,46 @@ return [
 
     'session' => [
 
+        /*
+        |--------------------------------------------------------------------------
+        | Session Driver
+        |--------------------------------------------------------------------------
+        |
+        | Select the class to be used to handle the session data.
+        |
+        | Supported: Coreplex\Core\Session\Native
+        |            Coreplex\Core\Session\Illuminate
+        |
+        */
+
         'driver' => 'Coreplex\Core\Session\Native',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Session Key
+        |--------------------------------------------------------------------------
+        |
+        | The key to store session variables against.
+        |
+        */
 
         'key' => 'coreplex.session'
 
     ],
 
     'renderer' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Renderer Driver
+        |--------------------------------------------------------------------------
+        |
+        | Select the class to be used to render data.
+        |
+        | Supported: Coreplex\Core\Renderer\Native
+        |            Coreplex\Core\Renderer\Illuminate
+        |
+        */
 
         'driver' => 'Coreplex\Core\Renderer\Native'
 
