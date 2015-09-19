b8771f5df1df84868c137ad1461c0fad2f8fc2e2
Michael Jennings <michael.jennings91@gmail.com>
2015-09-19 23:07:02 +0100

    Added the native and illuminate session classes

---
+++ src/Session/Native.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Coreplex\Core\Session;
+
+use Coreplex\Core\Contracts\Session;
+
+class Native implements Session
+{
+    /**
+     * The notifier config.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * An array of flashed data.
+     *
+     * @var array
+     */
+    protected $flash = [];
+
+    /**
+     * Flag to state if this is the first time the class has be instantiated.
+     *
+     * @var bool
+     */
+    protected $initialLoad = true;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+
+        if ( ! isset($_SESSION)) {
+            session_start();
+        }
+
+        if ($this->initialLoad) {
+            if (
+                isset($_SESSION[$this->config['session']['key']]) &&
+                isset($_SESSION[$this->config['session']['key']]['flash'])
+            ) {
+                $this->flash = $_SESSION[$this->config['session']['key']]['flash'];
+                unset($_SESSION[$this->config['session']['key']]['flash']);
+            }
+            $this->initialLoad = false;
+        }
+    }
+
+    /**
+     * Check if an item exists in the session.
+     *
+     * @param $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        $session = $this->getSessionData();
+
+        return array_key_exists($key, $session);
+    }
+
+    /**
+     * Retrieve a property from the session by its key.
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $session = $this->getSessionData();
+
+        return $this->has($key) ? $session[$key] : null;
+    }
+
+    /**
+     * Put an item in the session.
+     *
+     * @param $key
+     * @param $value
+     */
+    public function put($key, $value)
+    {
+        return $_SESSION[$this->config['session']['key']][$key] = $value;
+    }
+
+    /**
+     * Remove an item from the session.
+     *
+     * @param $key
+     */
+    public function forget($key)
+    {
+        if (
+            isset($_SESSION[$this->config['session']['key']]) &&
+            isset($_SESSION[$this->config['session']['key']][$key])
+        ) {
+            unset($_SESSION[$this->config['session']['key']][$key]);
+        }
+
+        if (isset($this->flash[$key])) {
+            unset($this->flash[$key]);
+        }
+    }
+
+    /**
+     * Flash an item to the session.
+     *
+     * @param $key
+     * @param $value
+     */
+    public function flash($key, $value)
+    {
+        $_SESSION[$this->config['session']['key']]['flash'][$key] = $value;
+        $this->flash[$key] = $value;
+    }
+
+    /**
+     * Merge all of the notifier session data and any flashed data.
+     *
+     * @return array
+     */
+    protected function getSessionData()
+    {
+        $data = isset($_SESSION[$this->config['session']['key']]) ? $_SESSION[$this->config['session']['key']] : [];
+
+        return array_merge($data, $this->flash);
+    }
+}
